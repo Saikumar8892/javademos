3e73ab1af0bd8366e99f5e887f79f7084a5bb916
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-07-14 09:52:17 +0530

    changes

---
+++ 01-helloworld/src/charAndBooleans.java
@@ -0,0 +1,24 @@
+
+public class charAndBooleans {
+
+	public static void main(String[] args) {
+		char myChar = 'D';
+		System.out.println("MyChar:" + myChar);
+		
+		int i = myChar;
+		System.out.println(i);
+		
+		char x = 69;
+		System.out.println("char x:" + x);
+		
+		char mynewchar = '\u0044';
+		System.out.println(mynewchar);
+		
+		mynewchar = '\u00A9';
+		System.out.println(mynewchar);
+		
+		boolean flag = true;
+		System.out.println(flag);
+		
+	}
+}
--- 01-helloworld/src/helloworld.java
+++ 01-helloworld/src/helloworld.java
@@ -4,4 +4,6 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	
+	
 }
