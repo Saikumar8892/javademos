1f10cfd09f27f5685ba3c9eced21e9c3fcdcf265
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-07-19 18:55:11 +0530

    changes

---
+++ Java Project/src/assistedproject1/TypeCasting.java
@@ -0,0 +1,26 @@
+package assistedproject1;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int a = 20;
+		System.out.println("value of:"+a);
+		long l = a;
+		System.out.println("value of:"+l);
+		double d = a;
+		System.out.println("value of:"+d);
+		float f = a;
+		System.out.println("value of:"+f);
+		
+		
+		double data = 28.8;
+		System.out.println("double value:"+data);
+		int num =(int)data;
+		System.out.println("int value:"+num);
+		long p = (long)data;
+		System.out.println("long value:"+p);
+		float s = (float)data;
+		System.out.println("float value:"+s);
+		
+	}
+}
