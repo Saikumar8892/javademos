33a97aac4a5fdeadd253643788a24f6c13bbd523
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-07-23 22:30:49 +0530

    changes

---
+++ Java Project/src/assistedproject9/ArrayExample2.java
@@ -0,0 +1,17 @@
+package assistedproject9;
+
+public class ArrayExample2 {
+
+	 public static void main(String[] args) {
+	        int a[]=new int[10];
+	        int i;
+	        for(i=0;i<a.length;i++)
+	        {
+	            a[i]=i+1;
+	        }
+	        for(i=0;i<10;i++)
+	        {
+	            System.out.println(a[i]);
+	        }
+	    }
+}
