4eaab42ca7b27ccd1a5dfbeb63aaa682586f353e
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-07-23 17:59:09 +0530

    changes

---
+++ Java Project/src/assistedproject10/RegularExpression.java
@@ -0,0 +1,19 @@
+package assistedproject10;
+
+import java.util.regex.*;
+public class RegularExpression {
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile("..m");
+		Matcher m = p.matcher("aim");
+		boolean b = m.matches();
+		System.out.println(b);
+	
+
+	    boolean b2 = Pattern.matches(".m", "a");
+	    System.out.println(b2);
+	    
+	    System.out.println(Pattern.matches("[Sai]", "i"));
+	    System.out.println(Pattern.matches("simpli", "j"));
+	    System.out.println(Pattern.matches("[NBK]", "B"));
+  }
+}
